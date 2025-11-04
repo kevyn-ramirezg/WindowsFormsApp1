345f67bd4f1f7003f35780042c4b079d080b914b
Kevyn <kevyn.ramirezg@uqvirtual.edu.co>
2025-11-04 14:50:23 -0500

    Revert "Avance 1"
    This reverts commit 76b0ddf4a730dd4ea7f81473917bde2137ea2a9b.

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,102 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.btnProductos = new System.Windows.Forms.Button();
-            this.btnClientes = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnProbar = new System.Windows.Forms.Button();
-            this.btnVentas = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(247, 278);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(130, 60);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Categorías";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.btnCategorias_Click);
-            // 
-            // btnProductos
-            // 
-            this.btnProductos.Location = new System.Drawing.Point(54, 278);
-            this.btnProductos.Name = "btnProductos";
-            this.btnProductos.Size = new System.Drawing.Size(123, 60);
-            this.btnProductos.TabIndex = 1;
-            this.btnProductos.Text = "Productos";
-            this.btnProductos.UseVisualStyleBackColor = true;
-            this.btnProductos.Click += new System.EventHandler(this.btnProductos_Click);
-            // 
-            // btnClientes
-            // 
-            this.btnClientes.Location = new System.Drawing.Point(438, 278);
-            this.btnClientes.Name = "btnClientes";
-            this.btnClientes.Size = new System.Drawing.Size(128, 60);
-            this.btnClientes.TabIndex = 2;
-            this.btnClientes.Text = "Clientes";
-            this.btnClientes.UseVisualStyleBackColor = true;
-            this.btnClientes.Click += new System.EventHandler(this.btnClientes_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
-            this.label1.Location = new System.Drawing.Point(260, 100);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(237, 33);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Productos ORLO";
-            // 
-            // btnProbar
-            // 
-            this.btnProbar.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.btnProbar.Location = new System.Drawing.Point(311, 369);
-            this.btnProbar.Name = "btnProbar";
-            this.btnProbar.Size = new System.Drawing.Size(130, 41);
-            this.btnProbar.TabIndex = 4;
-            this.btnProbar.Text = "Probar conexion";
-            this.btnProbar.UseVisualStyleBackColor = false;
-            this.btnProbar.Click += new System.EventHandler(this.btnProbar_Click);
-            // 
-            // btnVentas
-            // 
-            this.btnVentas.Location = new System.Drawing.Point(616, 278);
-            this.btnVentas.Name = "btnVentas";
-            this.btnVentas.Size = new System.Drawing.Size(122, 60);
-            this.btnVentas.TabIndex = 5;
-            this.btnVentas.Text = "Ventas";
-            this.btnVentas.UseVisualStyleBackColor = true;
-            this.btnVentas.Click += new System.EventHandler(this.btnVentas_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.HotTrack;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnVentas);
-            this.Controls.Add(this.btnProbar);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnClientes);
-            this.Controls.Add(this.btnProductos);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Principal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button btnProductos;
-        private System.Windows.Forms.Button btnClientes;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnProbar;
-        private System.Windows.Forms.Button btnVentas;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,8 +1,12 @@
-﻿using Oracle.ManagedDataAccess.Client;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using WindowsFormsApp1.Data;
-using WindowsFormsApp1.Forms;
 
 namespace WindowsFormsApp1
 {
@@ -13,52 +17,9 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        private void btnCategorias_Click(object sender, EventArgs e)
-        {
-            using (var f = new FormCategorias())
-            {
-                f.ShowDialog(this);
-            }
-        }
-
-        private void btnProductos_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
-            using (var f = new FormProductos())
-                f.ShowDialog(this);
         }
-
-        // Clientes
-        private void btnClientes_Click(object sender, EventArgs e)
-        {
-            using (var f = new FormClientes())
-                f.ShowDialog(this);
-        }
-
-        private void btnProbar_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                using (var cn = Db.Open())
-                using (var cmd = new OracleCommand("SELECT USER FROM dual", cn))
-                {
-                    var who = (string)cmd.ExecuteScalar();
-                    MessageBox.Show("Conectado como: " + who);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error de conexión: " + ex.Message);
-            }
-        }
-
-        private void btnVentas_Click(object sender, EventArgs e)
-        {
-            using (var f = new FormVenta())
-            {
-                f.ShowDialog(this);
-            }
-        }
-
     }
 }
